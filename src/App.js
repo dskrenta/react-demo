4767d7bbbbae957c0776b12752245bd9430e9a44
dskrenta <dskrenta@gmail.com>
2017-03-30 17:02:52 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>{this.props.message}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
