829cbfd12f0ba84bdd04dcf12fd04f3b1972ddd4
dskrenta <dskrenta@gmail.com>
2017-05-21 16:47:26 -0700

    removed react-crop & various cleanup changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.css';
-import ReactCrop from 'react-image-crop';
-import '../node_modules/react-image-crop/dist/ReactCrop.css';
 
 export default class App extends React.Component {
   constructor (props) {
@@ -13,7 +11,6 @@ export default class App extends React.Component {
     return (
       <div>
         <h2>Hello, World!</h2>
-        <ReactCrop src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ-YNEZDF5RGCpG5ytYp7ktwxYMHb3WMtoxNbFV8avGRuVH1nuyVw" />
       </div>
     );
   }
