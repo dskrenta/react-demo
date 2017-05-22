9fbb7544e0dc87e22bf960b6fd5ca1d5a7c5c4b1
dskrenta <dskrenta@gmail.com>
2017-05-22 02:06:27 -0700

    various ui updates

--- src/MaterialApp.js
+++ src/MaterialApp.js
@@ -16,23 +16,27 @@ const MaterialApp = () => (
 );
 
 const GridDemo = () => (
-  <GridList cols={6}>
-    <GridTile cols={5} style={gridTileStyle}>
-      <Card>
-        <CardMedia style={{padding: 0}}>
-          <img src="http://placehold.it/150x10" />
-        </CardMedia>
-      </Card>
-    </GridTile>
-    <GridTile cols={1} style={gridTileStyle}>
-      <Slider />
-      <Slider defaultValue={0.5} />
-      <Slider defaultValue={1} />
-    </GridTile>
-    <GridTile cols={6} style={gridTileStyle}>
-      List of images
-    </GridTile>
-  </GridList>
+  <div>
+    <GridList cols={6} style={{height: '80vh'}}>
+      <GridTile cols={5} style={gridTileStyle}>
+        <Card>
+          <CardMedia style={{padding: 0}}>
+            <img src="http://placehold.it/150x10" />
+          </CardMedia>
+        </Card>
+      </GridTile>
+      <GridTile cols={1} style={gridTileStyle}>
+        <Slider />
+        <Slider defaultValue={0.5} />
+        <Slider defaultValue={1} />
+      </GridTile>
+    </GridList>
+    <GridList cols={1} style={{height: '20vh'}}>
+      <GridTile cols={1} style={gridTileStyle}>
+        List of images
+      </GridTile>
+    </GridList>
+  </div>
 );
 
 export default MaterialApp;
